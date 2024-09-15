542f15f61336238bd00c6116c4dda8d4675513b4
levociraptor <leva.uspensky@gmail.com>
2024-09-15 12:58:00 +0400

    done with first exercise

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,10 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    assert build_url('my_site', 'goods') == 'my_site/goods'
+    params = {
+        'honor': 'smartphones',
+        'iphone': 'smartphones',
+        'samsung': 'TV'
+    }
+    assert (build_url('my_site', 'goods', params) == 'my_site/goods?honor=smartphones&iphone=smartphones&samsung=TV')
--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,15 @@
 from functions.level_1.two_date_parser import compose_datetime_from
+from datetime import datetime, time, date, timedelta
 
 
 def test_compose_datetime_from():
-    pass
+    assert compose_datetime_from('', '10:30') == datetime.combine(
+        date.today(),
+        time(10, 30)
+    )
+
+    tomorrow_date = date.today() + timedelta(days=1)
+    assert compose_datetime_from('tomorrow', '13:04') == datetime.combine(
+        tomorrow_date,
+        time(13, 4)
+    )
